da524a98945b0df97731874fb598e56c6349b540
Tayyab975 <mtayyeb975@gmail.com>
2024-05-08 09:55:27 +0400

    Testing

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PUSHING CODE 08-05-2024"
+include(":app")
+ 
